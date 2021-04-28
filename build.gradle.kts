f5739c989f825cfef5bf822ce5bdcd493ee7e2ca
Emanuel Moecklin <emanuel.moecklin@gmail.com>
2021-04-28 18:57:50 -0400

    minor cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     // for unknown reasons we need to use the fully qualified name for the plugin versions
     // importing com.onegravity.Plugin doesn't help
@@ -18,7 +20,7 @@ allprojects {
         maven("https://jitpack.io")
     }
 
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    tasks.withType<KotlinCompile> {
         kotlinOptions {
             jvmTarget = "1.8"
             freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn", "-Xuse-experimental=kotlin.ExperimentalStdlibApi")
