35295a9d37f3a998136ce195e4d0afac34e643aa
Emanuel Moecklin <emanuel.moecklin@1gravity.com>
2021-05-05 17:46:27 -0400

    some initial experiments, not working, not even compiling

---
+++ account-service/src/main/kotlin/com/onegravity/accountservice/persistence/database/Database2.kt
@@ -0,0 +1,4 @@
+package com.onegravity.accountservice.persistence.database
+
+class Database2 {
+}
---
+++ account-service/src/main/kotlin/com/onegravity/accountservice/persistence/database/DatabaseOperations.kt
@@ -0,0 +1,4 @@
+package com.onegravity.accountservice.persistence.database
+
+class DatabaseOperations {
+}
